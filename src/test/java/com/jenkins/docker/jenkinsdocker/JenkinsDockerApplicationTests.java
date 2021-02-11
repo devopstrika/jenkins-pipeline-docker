5fae007bcd00f5c67b39f624b152aa516993851b
innorfui <46398617+innorfui@users.noreply.github.com>
2021-02-11 22:44:22 +0400

    First commit

---
+++ src/test/java/com/jenkins/docker/jenkinsdocker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.docker.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
