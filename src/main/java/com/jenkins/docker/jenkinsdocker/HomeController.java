e5f9662136036b9c8139667689478fc1f0f0c2fd
Devopstrika <devopstrika@gmail.com>
2021-02-11 23:01:55 +0400

    Adding controller

---
+++ src/main/java/com/jenkins/docker/jenkinsdocker/HomeController.java
@@ -0,0 +1,14 @@
+package com.jenkins.docker.jenkinsdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/welcome")
+	public String greeting() {
+		
+		return "Welcome to my service";
+	}
+}
